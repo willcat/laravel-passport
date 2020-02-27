cf18b6911cb4f2bd68f7e55f6d6dbc9cb5b5bee8
yunxiuhe <yunxiuhe@100tal.com>
2020-02-27 20:48:48 +0800

    add passport

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,9 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('register', 'API\RegisterController@register');
+Route::post('login', 'API\RegisterController@login');
+
+Route::middleware('auth:api')->group( function () {
+    Route::resource('products', 'API\ProductController');
 });
